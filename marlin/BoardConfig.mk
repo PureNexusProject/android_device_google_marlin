cfe16006ad5af65d56127be2b63b5910f70942c3
beanstown106 <nbenis106@gmail.com>
2016-12-18 23:52:10 -0500

    common: build kernel inline *lets build pure kernel
    Change-Id: Ie9c21bfe1145fba5a191bc6d750dedea87f5d50b

--- marlin/BoardConfig.mk
+++ marlin/BoardConfig.mk
@@ -115,6 +115,13 @@ endif
 
 BOARD_EGL_CFG := device/google/marlin/egl.cfg
 
+# Inline kernel building
+KERNEL_TOOLCHAIN := $(ANDROID_BUILD_TOP)/prebuilts/gcc/$(HOST_OS)-x86/aarch64/aarch64-linux-android-4.9/bin
+KERNEL_TOOLCHAIN_PREFIX := aarch64-linux-android-
+TARGET_KERNEL_SOURCE := kernel/google/marlin
+TARGET_KERNEL_CONFIG := pure_defconfig
+BOARD_KERNEL_IMAGE_NAME := Image.lz4-dtb
+
 BOARD_KERNEL_BASE        := 0x80000000
 BOARD_KERNEL_PAGESIZE    := 4096
 ifneq ($(filter marlin_kasan, $(TARGET_PRODUCT)),)
